ed1a12a0e541afa761a4136c4a832153a1a873bd
Xander Siruno-Nebel <73612166+xandervsn@users.noreply.github.com>
2022-07-04 18:28:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,97 @@
 const api = 'https://ws.audioscrobbler.com/2.0/?format=json&';
-const apiKey = 'api_key=8c26f6d976a330378d34462eb67918b2';
-const method = 'method=track.getInfo&'
+const apiKey = 'api_key=KEY';
+const limit = 'limit=200&'
+const period = "period=overall&"
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'KEY',
+		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+	}
+};
 
-function findDuration(artist, track){
-    url = api + artist + track + method + apiKey
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            millisecs = data.track.duration
-            secs = millisecs/1000
-            seconds = secs%60
-            if (seconds.toString().length == 1){seconds = '0' + seconds}
-            minutes = (secs-seconds)/60
-            document.getElementById('output').innerHTML = minutes + ":" + seconds
-        })
+//global vars because i cba to call a function with 20 args
+let error = false;
+let failLog = ""
+let failCount = 0;
+let total = 0;
+let pre = 0;
+let post = 0;
+let num = 0;
+
+async function get(user){
+    const method = 'method=user.getInfo&'
+    const url = api + user + method + apiKey;
+    const res = await fetch(url);
+    const data = await res.json()
+    const playcount = data.user.playcount
+    const pages = (playcount-(playcount%200))/200 + 1
+    loop(user, pages)
+}
+
+async function loop(user, pages){
+    const method = 'method=user.gettoptracks&'
+    for (let i = 0; i < pages; i++) {
+        let page = 'page=' + i+1 + '&'
+        const url = api + user + page + period + method + limit + apiKey
+        await sleep(1000)
+        const res = await fetch(url)
+        const data = await res.json()
+        for (let j = 0; j < data.toptracks.track.length; j++) {
+            const playcount = data.toptracks.track[j].playcount
+            const artistRaw = data.toptracks.track[j].artist.name
+            const trackRaw = data.toptracks.track[j].name
+            const regex = /\(|\[|ft|FT|Ft|FEAT|feat|Feat/
+            const artist = artistRaw.split(regex)[0]
+            const track = trackRaw.split(regex)[0]
+            //const artist = encodeURIComponent(artistRaw = artistRaw.split('(')[0])
+            //const track = encodeURIComponent(trackRaw = trackRaw.split('(')[0])
+            let duration = data.toptracks.track[j].duration
+            if(duration == 0){
+                try{
+                    const deezerRes = await fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=' + artist + " " + track, options)
+                    const deezerData = await deezerRes.json()
+                    duration = deezerData.data[0].duration
+                }catch{
+                    failLog = failLog + artist + " - " + track+ "\n" 
+                    failCount += parseInt(playcount);
+                    error = true;
+                }
+            }
+            if(!error){
+                const durationTotal = duration*playcount
+                total += durationTotal
+                format(total)
+                post = Date.now()
+                num++
+                document.getElementById('timeListened').textContent = format(total)
+                document.getElementById('trackNumber').textContent = num
+                document.getElementById('errors').textContent = failCount
+                document.getElementById('errorList').textContent = failLog
+                //console.log("Time Listened: " + format(total))
+                //console.log("Runtime: " + ((post-pre)/1000) + " sec")
+                //console.log("Track Num: " + num + ", " + data.toptracks.track[j].name)
+            }else{
+                error = false;
+            }
+        }
+    }
+}
+
+function format(total){
+    let seconds = total%60
+    if (seconds.toString().length == 1){
+        seconds = '0' + seconds
+    }
+    let minutes = (total-seconds)/60
+    return minutes + ":" + seconds
 }
 
 document.getElementById('button').onclick = function onClick(){
-    let input = document.getElementById('textField').value
-    let artist = 'artist=' + input.split(' - ')[0] + '&'
-    let track = 'track=' + input.split(' - ')[1] + '&'
-    findDuration(artist, track);
+    const user = 'user=' + document.getElementById('textField').value + '&'
+    get(user);
+    pre = Date.now()
 }
