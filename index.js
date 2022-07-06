53169539909e83365a5472203759a5eb9f5d6434
Xander Siruno-Nebel <73612166+xandervsn@users.noreply.github.com>
2022-07-05 22:31:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -67,10 +67,11 @@ async function loop(user, pages){
                 format(total)
                 post = Date.now()
                 num++
-                document.getElementById('timeListened').textContent = format(total)
+                document.getElementById('timeListened').textContent = anal(total)
                 document.getElementById('trackNumber').textContent = num
                 document.getElementById('errors').textContent = failCount
                 document.getElementById('errorList').textContent = failLog
+                //console.log(`${artist} - ${track}: ${duration} sec, ${playcount} plays, ${durationTotal} total`)
                 //console.log("Time Listened: " + format(total))
                 //console.log("Runtime: " + ((post-pre)/1000) + " sec")
                 //console.log("Track Num: " + num + ", " + data.toptracks.track[j].name)
@@ -81,12 +82,34 @@ async function loop(user, pages){
     }
 }
 
+function anal(total){
+    const parent = new Date(total * 1000).toISOString().substring(5, 19);
+    const seconds = parent.split(':')[2]
+    const minutes = parent.split(':')[1]
+    const hours = parent.substring(6, 8)
+    const days = parent.substring(3, 5)
+    const months = parseInt(parent.substring(0, 2)) - 1
+    let lettered;
+    let analog;
+    if(months > 0){
+        lettered = `${months} months, ${days} days, ${hours} hr, ${minutes} min, ${seconds} sec`
+        analog = `${months}:${days}:${hours}:${minutes}:${seconds}`
+    }else{
+        lettered = `${days} days, ${hours} hr, ${minutes} min, ${seconds} sec`
+        analog = `${days}:${hours}:${minutes}:${seconds}`
+    }
+    return `${lettered}\n${analog}`
+}
+
 function format(total){
     let seconds = total%60
     if (seconds.toString().length == 1){
         seconds = '0' + seconds
     }
     let minutes = (total-seconds)/60
+    if (minutes.toString().length == 1){
+        minutes = '0' + seconds
+    }
     return minutes + ":" + seconds
 }
 
